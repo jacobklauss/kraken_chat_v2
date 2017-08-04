9b27af2d652a030b785aed3675d5e612ff674b4c
Jordan Jantschulev <j.jantschulev@gmail.com>
2017-08-04 20:55:20 +1000

    socket.io working

---
+++ server.js
@@ -0,0 +1,14 @@
+const PORT = 3003;
+
+var express = require('express');
+var app = express();
+var server = app.listen(PORT,function () {
+  console.log("kraken chat v2 server running on port: "+PORT);
+});
+app.use(express.static('public'));
+
+var io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+  console.log("new connected "+socket.id);
+})
